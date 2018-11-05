d9881ef7d4163d15f176323874129f30aa04345e
Zachary Vanscoit <zachary816@gmail.com>
2018-11-05 16:24:23 -0600

    Added database project
    added the actual database in mysql, and added a test project to work with JDBC.

---
+++ JDBC-Test/src/com/run/JDBC_Test_Launcher.java
@@ -0,0 +1,23 @@
+package com.run;
+
+import java.sql.*;
+
+public class JDBC_Test_Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("Starting the JDBC test.\n");
+		try {
+			// 1. Get a connection
+			
+			// 2. Create a statement
+			
+			// 3. Execute SQL query
+			
+			// 4. Process result set
+			
+		} catch(Exception e) {
+			System.out.println("\n\n--------------------------\nWhoops. Something happened...\n");
+		}
+	}
+
+}
